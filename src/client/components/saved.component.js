79d243142329df8be88ddaf47830af2f951f5153
krosenk729 <krosenk729@gmail.com>
2018-04-02 11:33:48 -0400

    three command prompt windows open and I'm making progress ... updated saved articles to pull in on angular side from mongo db

--- src/client/components/saved.component.js
+++ src/client/components/saved.component.js
@@ -1,6 +1,13 @@
 angular.module("savedArticles", []).component("savedArticles", {
 	templateUrl: "templates/saved.template.html",
-	controller: function SavedArticlesController(){
+	controller: function SavedArticlesController($http){
 		console.log('saved component');
+		this.saved = [];
+
+		$http.get('/api/saved').then( data => {
+			this.saved = data.data;
+			console.log(data.data);
+		});
+
 	}
 });
