7153d16161e418d8b0068703264d611a776d32c0
krosenk729 <krosenk729@gmail.com>
2018-04-02 10:39:45 -0400

    done with index.html

--- src/client/app.module.js
+++ src/client/app.module.js
@@ -12,33 +12,5 @@ angular.module("onionQuiz", [
 		template: "<scraped-articles></scraped-articles>"
 	}).when("/saved", {
 		template: "<saved-articles></saved-articles>"
-	});
+	}).otherwise("/quiz");
 }]);
-
-/*
-
-angular.module("onionQuiz", [
-	"ngRoute",
-	"quiz",
-	"savedArticles",
-	"scrapedArticles"
-])
-.config(["$locationProvider", "$routeProvider"],
-function config($locationProvider, $routeProvider){
-	$locationProvider.html5Mode(true);
-	
-	$routeProvider
-	.when("/quiz", {
-		template: "<quiz></quiz>"
-	})
-	.when("/scrapes", {
-		template: "<scraped-articles></scraped-articles>"
-	})
-	.when("/saves", {
-		template: "<saved-articles></saved-articles>"
-	});
-	// .otherwise("/quiz");
-
-});;
-
-*/
