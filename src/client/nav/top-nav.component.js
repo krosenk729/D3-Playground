c5c58b57ae3536377579fc7b21808eb202c60893
krosenk729 <krosenk729@gmail.com>
2018-04-07 09:05:13 -0400

    fixed dthe mobile issues that made me sad

--- src/client/nav/top-nav.component.js
+++ src/client/nav/top-nav.component.js
@@ -4,6 +4,10 @@ angular.module("onionQuiz").component("topNav", {
 	<div class="play">
 	<a href="#/quiz">Play "Guess The Onions"</a>
 	</div>
+	<div class="d-none d-md-block">
+	<a href="#/scraped">See All Scrapes (Spoiler Alert)</a>
+	<a href="#/saved">Saved Articles</a>
+	</div>
 	</nav>
 	`,
 	controller: function AppNavController(){ }
