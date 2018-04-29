f5cf87b5c5f0cf1e675db9a68fa56b6aec0e9c0c
krosenk729 <krosenk729@gmail.com>
2018-04-29 07:08:05 -0400

    added alphabet test

--- src/client/scripts.js
+++ src/client/scripts.js
@@ -1,14 +1,12 @@
 const getAllAlphabet = function(str) {
 	const founds = {};
 	const alpha = 'ABCDEFHIJKLMNOPQRSTUVWXYZ'
-	.split('')
-	.forEach(letter => founds[letter] = 0);
-
-
-
+		.split('')
+		.forEach(letter => founds[letter] = 0);
+	str.split('')
+		.filter(character => character.match(/[A-Z]/i))
+		.forEach(letter => founds[letter.toUpperCase()]++);
 	return founds;
 }
 
-console.log(getAllAlphabet('katheirne'));
-
-module.exports = {getAllAlphabet};
+module.exports = { getAllAlphabet };
--- test/client.scripts.test.js
+++ test/client.scripts.test.js
@@ -3,7 +3,7 @@ const chai = require("chai");
 // =============================================================
 // Setting up scripts to be tested
 
-const client = require("../client/scripts");
+const client = require("../src/client/scripts");
 
 // =============================================================
 // Setting up chai
@@ -17,15 +17,18 @@ describe("getAllAlphabet", function(){
   });
 
   it("should have one property per letter of the alphabet", () => {
-
+  	assert.hasAllKeys(client.getAllAlphabet("Hello World"), 'ABCDEFHIJKLMNOPQRSTUVWXYZ'.split(''));
   });
 
   it("should count the number of letters in a string", () => {
-
+  	assert.containsAllKeys(client.getAllAlphabet("Hello"), {"H": 1,"E": 1, "L": 2, "O": 1});
+  	assert.containsAllKeys(client.getAllAlphabet("AAABBBbbbccc"), {"A": 3,"B": 6, "C": 3});
   });
 
   it("should be stable regardless of inputs", () => {
-
+  	assert.isOk(client.getAllAlphabet(""), "Empty input");
+  	assert.isOk(client.getAllAlphabet("123123"), "Number input");
+  	assert.isOk(client.getAllAlphabet("<#@!"), "Special character input");
   });
 
 });
